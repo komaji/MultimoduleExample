bd58d5983f9e16e7d3ea71922ec5d873aef461f2
kojikoji <kojima.t@pepabo.com>
2022-06-20 21:56:41 +0900

    異なる App Target を追加してみる

---
+++ MultiModuleExampleBeta/MultiModuleExampleBetaApp.swift
@@ -0,0 +1,18 @@
+//
+//  MultiModuleExampleBetaApp.swift
+//  MultiModuleExampleBeta
+//
+//  Created by kojima.t on 2022/06/20.
+//
+
+import SwiftUI
+import Main
+
+@main
+struct MultiModuleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView(resolver: MultiModuleBetaResolver())
+        }
+    }
+}
